154317da7b9d325229a253335bdf73159c8e6dc7
Resurt <mr.roman-07@ya.ru>
2020-04-19 00:35:35 +0300

    Создаём схему БД личного проекта

--- schema.sql
+++ schema.sql
@@ -1,26 +1,30 @@
-CREATE DATABASE doingsdone_db DEFAULT CHARACTER SET utf8;
+CREATE DATABASE doingsdone_db
+  DEFAULT CHARACTER SET utf8
+  DEFAULT COLLATE utf8_general_ci;
 
-CREATE TABLE `doingsdone_db`.`project` (
+USE doingsdone_db;
+
+CREATE TABLE `project` (
   `id` INT(11) NOT NULL AUTO_INCREMENT ,
-  `title` VARCHAR(255) NOT NULL ,
+  `title` CHAR(50) NOT NULL ,
   `user_id` INT(11) NOT NULL , PRIMARY KEY (`id`)
 ) ENGINE = InnoDB;
 
-CREATE TABLE `doingsdone_db`.`task` (
+CREATE TABLE `task` (
   `id` INT(11) NOT NULL AUTO_INCREMENT ,
   `pubdate` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ,
   `status` INT(11) NOT NULL DEFAULT '0' ,
-  `title` VARCHAR(255) NOT NULL ,
-  `link` TEXT NULL ,
+  `title` CHAR(50) NOT NULL ,
+  `link` CHAR(100) NULL ,
   `deadline` DATE NULL ,
   `user_id` INT(11) NOT NULL ,
   `project_id` INT(11) NOT NULL , PRIMARY KEY (`id`)
 ) ENGINE = InnoDB;
 
-CREATE TABLE `doingsdone_db`.`user` (
+CREATE TABLE `user` (
   `id` INT(11) NOT NULL AUTO_INCREMENT ,
   `reg_date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ,
-  `email` VARCHAR(100) NOT NULL ,
-  `name` VARCHAR(100) NOT NULL ,
-  `password` VARCHAR(50) NOT NULL , PRIMARY KEY (`id`)
+  `email` CHAR(50) NOT NULL ,
+  `name` CHAR(50) NOT NULL , UNIQUE (`email`) ,
+  `password` CHAR(64) NOT NULL , PRIMARY KEY (`id`)
 ) ENGINE = InnoDB;
